c2f8e0f7df832d6f732ac799c8fcb452b5a291a7
Palash Bauri <palashbauri1@gmail.com>
2022-03-15 12:05:59 +0530

    Fixed the same timestamp bug; cause is the speed of code

--- vlok.v
+++ vlok.v
@@ -7,7 +7,7 @@ struct Vlok{
 
 mut:
 	index int [required]
-	timestamp string [required]
+	timestamp i64
 	data string [required]
 	prev_hash string [required]
 	self_hash string
@@ -26,18 +26,47 @@ fn (v Vlok) to_string() string{
 
 }
 
+
+// Create a new block on the heap
 fn new_vlok(index int , data string , prev_hash string) &Vlok{
 	
-	t := time.utc()
-	timestamp := "$t.unix"
-	mut vlok_ := &Vlok{index: index, timestamp: timestamp, data: data, prev_hash: prev_hash}
+	
+	mut vlok_ := &Vlok{index: index, timestamp: time.utc().unix, data: data, prev_hash: prev_hash}
+	
 	vlok_.set_hash()
+
+	println("Block #$index => [$vlok_.to_string()]")
 	return vlok_
+
+}
+
+//genesis block creation
+fn create_genesis() &Vlok{
+	
+	return new_vlok(0 , "0" , "0")
+
+}
+
+
+// takes the previous block and returns new block;
+// this function creates a new block; previous block is necessary here
+fn create_block(prev &Vlok , data string) &Vlok{
+	
+	nv := new_vlok(prev.index + 1 , data , prev.self_hash )
+	return nv
+
 }
 
 
 fn main() {
 	genesis := new_vlok(0, "0" , "0")
-	v1 := new_vlok(1,"1", genesis.self_hash )
-	println('$genesis.to_string() \n $v1.to_string()')
+	st := time.Duration(1000000000) //1 second
+	mut chain := [genesis]
+	for i:=1; i<=10; i+=1{
+		
+		chain << create_block( chain[i-1] , "block number $i" )
+		
+		time.sleep(st) //If it doesn't sleep; most of the blocks will have the same time-stamp
+
+	}
 }
